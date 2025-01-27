516b7b3b3200fe170316f5f8a93c7af246eb217d
Diego Rubio Canales <diegorubiocanales2006@gmail.com>
2025-01-27 17:06:19 +0100

    Reparar error

--- simulation.py
+++ simulation.py
@@ -17,6 +17,16 @@ class Simulation:
         self.__scale = 5*10**5 # m / pixel
         self.__t_increment = 1000000 # time multiplier
         self.__dt = 1/60 # time unit between frames
+
+        self.__bod_1 = Particle(R3Vector(self.__scale * WIDTH/2, self.__scale
+                    * HEIGHT/2, 0), R3Vector(0,0, 0) ,
+                                0, 5.972 * 10**24, 6378000)
+
+        self.__bod_2 = Particle(
+            R3Vector(self.__scale * WIDTH/2 + 384400000, self.__scale * HEIGHT / 2, 0),
+            R3Vector(0, 1000, 0),
+            0, 7.349 * 10**22, 1740000)
+
         self.__bodies = [self.__bod_1, self.__bod_2]
 
         self.__space1 = Space(WIDTH, HEIGHT, self.__bodies)
@@ -83,8 +93,3 @@ class Simulation:
             raise ValueError("t_increment must be positive")
         else:
             self.__t_increment = increment
-
-class SunEarthMoonSimulation(Simulation):
-    pass
-    #TODO: Subclasses of Simulation are concrete models of simulatiom.
-    # Finish this one
